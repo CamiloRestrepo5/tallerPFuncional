cd35ba77ae7806af89b2b2ee49f93766b9488307
Feolinopio <feliperodriguez@upb.edu.co>
2025-11-16 19:29:53 -0500

    Finalización de los ejercicios y commit final

---
+++ ejercicio10.py
@@ -0,0 +1,16 @@
+# 1. ¿Cuál es el resultado de la operación?
+# El resultado de print(list(resultado)) es:
+# [12, 15, 18]
+
+
+# 2. ¿Qué hizo la función map junto con lambda paso a paso?
+# La función map tomó simultáneamente un elemento de cada lista (listal, lista2, lista3)
+# y la función lambda sumó esos tres valores.
+
+# Operaciones realizadas:
+# 1 + 4 + 7 = 12
+# 2 + 5 + 8 = 15
+# 3 + 6 + 9 = 18
+
+# Por eso el resultado final es la lista:
+# [12, 15, 18]
--- ejercicio9.py
+++ ejercicio9.py
@@ -1,11 +1,11 @@
-# 1. ¿Qué retorna el bloque de Código?
+# 1. ¿Qué retorna el bloque de código?
 # {1, 3, 4}
 
-# 2. ¿Cuántas funcionaes de declararon?
+# 2. ¿Cuántas funciones se declararon?
 # Se declararon 2 funciones:
 # agregar_elemento
 # eliminar_elemento
 
 # 3. ¿Qué imprime en consola?
 # No imprime nada, porque no hay ningún print en el código.
-# (Si hicieras print(resultado), mostraría {1, 3, 4}).
+# (Si se hace print(resultado), mostraría {1, 3, 4}).
